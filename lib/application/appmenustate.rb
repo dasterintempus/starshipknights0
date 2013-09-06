83de2e28670c052e470ac3122c4e04a696593cb2
Scott Buettner <buettner.scott@gmail.com>
2013-09-05 22:44:58 -0600

    Adding WASD and other key options for menus

--- lib/application/appmenustate.rb
+++ lib/application/appmenustate.rb
@@ -90,11 +90,27 @@ module StarshipKnights
           when Gosu::KbEscape
             @app.play_sound("menufail")
             @app.pop_state
+          when Gosu::KbLeftShift
+            @app.play_sound("menufail")
+            @app.pop_state
+          when Gosu::KbNumpad2
+            @app.play_sound("menufail")
+            @app.pop_state
           when Gosu::KbDown
             arrow_down
           when Gosu::KbUp
             arrow_up
+          when Gosu::KbS
+            arrow_down
+          when Gosu::KbW
+            arrow_up
           when Gosu::KbReturn
+            @app.play_sound("menuok")
+            select_option
+          when Gosu::KbSpace
+            @app.play_sound("menuok")
+            select_option
+          when Gosu::KbNumpad1
             @app.play_sound("menuok")
             select_option
         end
